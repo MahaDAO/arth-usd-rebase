ce272fa01f8a7888f4d9cb45224eb896e7547670
Steven Enamakel <enamakel@vezures.com>
2022-03-01 23:43:26 +0000

    updated test cases

--- test/index.ts
+++ test/index.ts
@@ -48,18 +48,90 @@ describe("ArthUSDWrapper", function () {
     expect(arthUsd).eq("0");
   });
 
-  it("Should perform deposit properly", async function () {
-    // try to deposit 1 token
-    const receipt = await arthUsdRebase.deposit(TENe18);
+  describe("when gmu is unchanged after a deposit", async function () {
+    beforeEach("deposit some tokens and increase the gmu", async () => {
+      const receipt = await arthUsdRebase.deposit(TENe18);
 
-    await expect(receipt)
-      .to.emit(arthUsdRebase, "Deposit")
-      .withArgs(operator.address, TENe18);
+      await expect(receipt)
+        .to.emit(arthUsdRebase, "Deposit")
+        .withArgs(operator.address, TENe18);
+    });
 
-    const arthR = await arth.balanceOf(operator.address);
-    expect(arthR).eq(TENe18.mul("999999999"));
+    it("Balances should be correct", async function () {
+      const arthR = await arth.balanceOf(operator.address);
+      expect(arthR).eq(TENe18.mul("999999999"));
 
-    const arthUsd = await arthUsdRebase.balanceOf(operator.address);
-    expect(arthUsd).eq(TENe18.mul(2)); // should report 2$
+      const arthUsd = await arthUsdRebase.balanceOf(operator.address);
+      expect(arthUsd).eq(TENe18.mul(2)); // should report 2$
+    });
+
+    it("Should perform withdraw properly", async function () {
+      // try to deposit 1 token
+      const receipt = await arthUsdRebase.withdraw(TENe18);
+
+      await expect(receipt)
+        .to.emit(arthUsdRebase, "Withdraw")
+        .withArgs(operator.address, TENe18);
+
+      const arthR = await arth.balanceOf(operator.address);
+      expect(arthR).eq(TENe18.mul("1000000000"));
+
+      const arthUsd = await arthUsdRebase.balanceOf(operator.address);
+      expect(arthUsd).eq("0");
+    });
+  });
+
+  describe("when gmu is increased after a deposit", async function () {
+    beforeEach("deposit some tokens and increase the gmu", async () => {
+      await arthUsdRebase.deposit(TENe18);
+      await gmuOracle.setPrice(2100000, 6);
+    });
+
+    it("Balances should increase", async function () {
+      const arthUsd = await arthUsdRebase.balanceOf(operator.address);
+      expect(arthUsd).eq(TENe18.mul(21).div(10)); // should report 2.1$
+    });
+
+    it("Withdraw should give back the principal amount", async function () {
+      // try to deposit 1 token
+      const receipt = await arthUsdRebase.withdraw(TENe18);
+
+      await expect(receipt)
+        .to.emit(arthUsdRebase, "Withdraw")
+        .withArgs(operator.address, TENe18);
+
+      const arthR = await arth.balanceOf(operator.address);
+      expect(arthR).eq(TENe18.mul("1000000000"));
+
+      const arthUsd = await arthUsdRebase.balanceOf(operator.address);
+      expect(arthUsd).eq("0"); // should report 2$
+    });
+  });
+
+  describe("when gmu is decreased after a deposit", async function () {
+    beforeEach("deposit some tokens and increase the gmu", async () => {
+      await arthUsdRebase.deposit(TENe18);
+      await gmuOracle.setPrice(1500000, 6);
+    });
+
+    it("Balances should increase", async function () {
+      const arthUsd = await arthUsdRebase.balanceOf(operator.address);
+      expect(arthUsd).eq(TENe18.mul(15).div(10)); // should report 1.5$
+    });
+
+    it("Withdraw should give back the principal amount", async function () {
+      // try to deposit 1 token
+      const receipt = await arthUsdRebase.withdraw(TENe18);
+
+      await expect(receipt)
+        .to.emit(arthUsdRebase, "Withdraw")
+        .withArgs(operator.address, TENe18);
+
+      const arthR = await arth.balanceOf(operator.address);
+      expect(arthR).eq(TENe18.mul("1000000000"));
+
+      const arthUsd = await arthUsdRebase.balanceOf(operator.address);
+      expect(arthUsd).eq("0"); // should report 2$
+    });
   });
 });

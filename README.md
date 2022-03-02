3515f7b504dc1b789894d216137ad0462ac07a65
Steven Enamakel <enamakel@vezures.com>
2022-03-02 00:01:22 +0000

    updated deployment script

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This repo implements a simple rebase token that is backed by ARTH collateral but
 
 This is a rebase token. The token automatically rebases itself to match the dollar value of the underlying ARTH that is locked.
 
-Since ARTH appreciates over time, users should see their rebase token balance only gradually increase over time.
+Since ARTH appreciates over time, users should see their rebase token balance only gradually increase over time. This makes `ARTH.usd` a positive rebase token very similar to how yield bearing tokens work.
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,37 +1,40 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
+// const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const arth = "0xb69a424df8c737a122d0e60695382b3eec07ff4b";
+  const gmuOracle = "0xdd465b9c68750a02c307744a749954b1f9787efb";
+  const governance = "0xeccE08c2636820a81FC0c805dBDC7D846636bbc4";
+  const chainId = 56;
 
-  const arth = "0x0";
-  const gmuOracle = "0x1";
-  const governance = "0x2";
-  const chainId = 32;
-
-  // We get the contract to deploy
-  const ArthUSDWrapper = await ethers.getContractFactory("ArthUSDWrapper");
-  const instance = await ArthUSDWrapper.deploy(
+  const constructorArguments = [
     "ARTH USD Rebase",
     "ARTH.usd",
     arth,
     gmuOracle,
     governance,
-    chainId
+    chainId,
+  ];
+
+  // We get the contract to deploy
+  const ArthUSDWrapper = await ethers.getContractFactory("ArthUSDWrapper");
+  const instance = await ArthUSDWrapper.deploy(
+    String(constructorArguments[0]),
+    String(constructorArguments[1]),
+    String(constructorArguments[2]),
+    String(constructorArguments[3]),
+    String(constructorArguments[4]),
+    constructorArguments[5]
   );
 
   await instance.deployed();
-
   console.log("ArthUSDWrapper deployed to:", instance.address);
+
+  await hre.run("verify:verify", {
+    address: "0x88fd584dF3f97c64843CD474bDC6F78e398394f4",
+    contract: "contracts/ArthUSDWrapper.sol:ArthUSDWrapper",
+    constructorArguments,
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
